d1680428c69252fb90019726d7053c5a0f745a04
sameergodara17 <sameergodara17@gmail.com>
2026-02-24 02:53:26 +0530

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Website from "./Website.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Website />
+  </React.StrictMode>
+);
